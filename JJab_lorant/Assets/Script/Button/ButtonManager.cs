3ea177a425d6175f70b5b28957e9501a4ff1b794
HAEBI <WindSlimeoo@gmail.com>
2025-12-12 21:12:20 +0900

    버튼매니저 추가가가가가각

---
+++ JJab_lorant/Assets/Script/Button/ButtonManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public float showDelay = 19f;
+
+    void Start()
+    {
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+    }
+
+    public void ShowButton()
+    {
+        StartCoroutine(Show());
+    }
+
+    IEnumerator Show()
+    {
+        yield return new WaitForSeconds(showDelay);
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Replay()
+    {
+        Scene current = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(current.name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
